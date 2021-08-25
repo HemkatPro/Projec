b344716761a024e3217931ed7664ef67bbd78dc5
HemkatPro <89548921+HemkatPro@users.noreply.github.com>
2021-08-25 16:24:58 -0400

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,5 @@
+var fruits = ["Apple", "Oragne", "Pear"];
+console.log(fruits);
+document.getElementById("decloration_of_arrays").innerHTML = fruits
+document.getElementById("decloration_of_arrays").innerHTML = fruits
+var Cars = fruits.length;
